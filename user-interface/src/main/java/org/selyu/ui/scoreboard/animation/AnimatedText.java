1cc6398712885377226076ada39c5ffeba75d96a
Lilian <lilyarmes@protonmail.com>
2020-07-01 23:38:42 -0400

    Add user-interface module for customizable scoreboards

---
+++ user-interface/src/main/java/org/selyu/ui/scoreboard/animation/AnimatedText.java
@@ -0,0 +1,62 @@
+package org.selyu.ui.scoreboard.animation;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class AnimatedText {
+    public static int frameUpdateSpeed = 3;
+
+    protected final List<String> framesList = new ArrayList<>();
+    protected int frameCount = 0;
+    protected int currentFrameIdx = 0;
+
+    public AnimatedText(@NotNull String... frames) {
+        framesList.addAll(Arrays.asList(frames));
+    }
+
+    public final void addFrame(@NotNull String frame) {
+        framesList.add(frame);
+    }
+
+    /**
+     * If there is more than one frame increment the frame count and then check
+     * if the count % {@link AnimatedText#frameUpdateSpeed} is zero, if so move to the next frame
+     */
+    public void processFrame() {
+        if (framesList.size() > 1) {
+            frameCount++;
+            if (frameCount % frameUpdateSpeed == 0) {
+                currentFrameIdx++;
+            }
+        }
+    }
+
+    public final String getCurrentFrame() {
+        if (currentFrameIdx >= framesList.size())
+            currentFrameIdx = 0;
+        return framesList.get(currentFrameIdx);
+    }
+
+    public final @NotNull List<String> getFramesList() {
+        return framesList;
+    }
+
+    public final int getFrameCount() {
+        return frameCount;
+    }
+
+    public final void setFrameCount(int frameCount) {
+        this.frameCount = frameCount;
+    }
+
+    public final int getCurrentFrameIdx() {
+        return currentFrameIdx;
+    }
+
+    public final void setCurrentFrameIdx(int currentFrameIdx) {
+        this.currentFrameIdx = currentFrameIdx;
+    }
+}
