adcf0c94eccba3ebfe3346123d37462b9867e21a
Lilian <lilyarmes@protonmail.com>
2020-07-11 20:02:16 -0400

    Start working on custom items

---
+++ core/src/main/kotlin/org/selyu/smp/core/item/item.kt
@@ -0,0 +1,23 @@
+package org.selyu.smp.core.item
+
+import org.bukkit.Material
+import org.bukkit.block.Block
+import org.bukkit.entity.Entity
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+
+interface CoreItem {
+    fun getItem(): ItemStack
+}
+
+@Retention
+@Target(AnnotationTarget.CLASS)
+annotation class ItemData(val material: Material, val modelData: Int)
+
+@Retention
+@Target(AnnotationTarget.FUNCTION)
+annotation class ItemEventHandler
+
+data class DamageEntityEvent(val player: Player, val itemStack: ItemStack, val damagedEntity: Entity)
+data class LeftClickEvent(val player: Player, val itemStack: ItemStack, val clickedEntity: Entity?, val clickedBlock: Block?)
+data class RightClickEvent(val player: Player, val itemStack: ItemStack, val clickedEntity: Entity?, val clickedBlock: Block?)
